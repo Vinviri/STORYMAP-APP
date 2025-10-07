0122af52cc6aa022c2ad9cfbb0a575f7aac44bb1
Vinviri <rafi.satriapradana@gmail.com>
2025-10-07 14:09:05 +0700

    Fix asset paths to be relative for deployment

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -105,7 +105,7 @@ window.addEventListener('hashchange', handleRoute);
 
 window.addEventListener('load', () => {
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js')
+    navigator.serviceWorker.register('sw.js')
       .then(() => console.log('Service Worker registered successfully.'))
       .catch(error => console.log('Service Worker registration failed:', error));
   }
